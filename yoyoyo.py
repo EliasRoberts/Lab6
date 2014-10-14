928000779f9c9bddcc7402a40a2ba17cc8a7ad54
EliasRoberts <eliasroberts99@gmail.com>
2014-10-14 14:05:03 -0500

    Lab6

---
+++ yoyoyo.py
@@ -0,0 +1,11 @@
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for qwerty in myList:
+    print "yeeeeeeeaaaaaaaaah"
+    print qwerty
+print "you good yo"
+
+for eek in range(11, 20, 1):
+    print eek
+print "super wow yo"
+
+superduperscooperlist = []
